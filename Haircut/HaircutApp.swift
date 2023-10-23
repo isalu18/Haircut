de3de34666a0110c8fcf0ae38ff79facba492074
Isaac Sanchez <isaacsanchez@mac-kun.local>
2023-10-23 16:28:58 -0700

    first commit

---
+++ Haircut/HaircutApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaircutApp.swift
+//  Haircut
+//
+//  Created by Isaac Sanchez on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HaircutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
